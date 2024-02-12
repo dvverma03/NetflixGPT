beba1d1c0bf7ec21971d05dfaaf449ea07e2a713
dvverma03 <dvverma9211@gmail.com>
2024-02-12 16:41:56 +0530

    sbcdef

--- src/utils/OpenAI.js
+++ src/utils/OpenAI.js
@@ -1,5 +1,11 @@
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
+<<<<<<< HEAD
   apiKey: process.env.REACT_APP_OPENAI_KEY, dangerouslyAllowBrowser: true 
 });
+=======
+  apiKey: process.env.REACT_APP_OPENAI_KEY,
+  dangerouslyAllowBrowser: true,
+})
+>>>>>>> 95b62d9 (sbcdef)
