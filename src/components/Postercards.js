587d378106f6e6d9886e23fee54292dd065a299c
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-02-14 03:08:10 +0530

    Update Postercards.js

--- src/components/Postercards.js
+++ src/components/Postercards.js
@@ -2,31 +2,28 @@ import React from 'react'
 import { Imag_URL } from '../utils/constant'
 import { useDispatch } from 'react-redux'
 import { addId } from '../utils/idSlice';
-// import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
-const Postercards = ({img, key}) => {
-
+const Postercards = (props) => {
   const dispatch= useDispatch();
-
-  function onClickHandler(){
-    return (
-     dispatch(addId(key))
-    )
+  const navigate=useNavigate();
+  const  onClickHandler = ()=>{
+    
+     dispatch(addId(props.id))
+     navigate("/videoplay")
   }
+  
 
   return (
-    // <Link
-    // to={`/videoplay/${key}`}
-    //           key={key}
-    // >
-    <div className='z-10 w-44 mx-2 my-4 bg-cover hover:w-48 hover:mx-0 hover:my-0 cursor-pointer'>
-        <img className='rounded-lg' src={Imag_URL + img} alt="" key={key} 
+    <div className='z-10 w-36 md:w-44 mx-2 my-4 bg-cover hover:w-40 md:hover:w-48 hover:mx-0 hover:my-0 cursor-pointer'>
+        <img className='rounded-lg' src={Imag_URL + props.poster_path
+} alt="" key={props.key} 
         onClick={onClickHandler}
         />
     </div>
-    // </Link>
   )
 }
 
 export default Postercards
+
