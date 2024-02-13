6030eb229c8629cab33081bd4957928421e3f089
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-02-13 09:02:17 +0530

    Update OpenAI.js

--- src/utils/OpenAI.js
+++ src/utils/OpenAI.js
@@ -1,12 +1,5 @@
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
-<<<<<<< HEAD
   apiKey: process.env.REACT_APP_OPENAI_KEY, dangerouslyAllowBrowser: true 
 });
-=======
-  apiKey: process.env.REACT_APP_OPENAI_KEY,
-  // apiKey: "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",
-  dangerouslyAllowBrowser: true,
-})
->>>>>>> 95b62d9 (sbcdef)
