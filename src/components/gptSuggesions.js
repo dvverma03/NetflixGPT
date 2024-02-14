40a938cdbc6c6235f50ab153ff1b01e328c866d6
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-02-15 03:02:53 +0530

    Update gptSuggesions.js

--- src/components/gptSuggesions.js
+++ src/components/gptSuggesions.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Postercards from './Postercards';
-
-const GptSuggestions = () => {
-  const { movieResults } = useSelector((store) => store.gpt);
-
-  if (!movieResults) return null;
+const GptSuggesions = () => {
+  const { movieResults, movieNames } = useSelector((store) => store.gpt);
+  if (!movieResults || !movieNames) return null;
 
   return (
-    <div className='bg-black bg-opacity-70 w-screen mt-6 box-border'>
-      <div className='bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
-        {movieResults.map((movies, index) => (
-          <div key={index} className='flex'>
-            <div className='flex overflow-x-auto w-screen scrollbar-hide'>
-              {movies.results.map((movie) => (
-               <Postercards {...movie} />
-              ))}
-            </div>
-          </div>
-        ))}
+    <div className=' bg-black bg-opacity-70 w-screen mt-6 box-border'>
+    <div className=' bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
+      {movieResults.map((movies, index) => (
+        <div key={index} className='flex '>
+             <div className='flex overflow-x-auto w-screen scrollbar-hide' >
+        <div className='flex'>
+              {
+                movies.results.map((movie)=>(<Postercards {...movie} />))
+            }
+        </div>
       </div>
+           
+        </div>
+      ))}
     </div>
   );
 };
-
-export default GptSuggestions;
-
+export default GptSuggesions;
