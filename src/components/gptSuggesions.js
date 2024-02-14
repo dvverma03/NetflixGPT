2374d6e9159c04e32cd1f801c74c22a17bd72ba1
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-02-15 00:22:08 +0530

    Update gptSuggesions.js

--- src/components/gptSuggesions.js
+++ src/components/gptSuggesions.js
@@ -2,76 +2,27 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Postercards from './Postercards';
 
-const GptSuggesions = () => {
-  const { movieResults, movieNames } = useSelector((store) => store.gpt);
-  if (!movieResults || !movieNames) return null;
+const GptSuggestions = () => {
+  const { movieResults } = useSelector((store) => store.gpt);
+
+  if (!movieResults) return null;
 
   return (
-    <div className=' bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
-      {movieResults.map((movies, index) => (
-        <div key={index} className='flex '>
-             <div className='flex overflow-x-auto w-screen scrollbar-hide' >
-        <div className='flex'>
-
-              {
-                movies.results.map((movie)=>(<Postercards img={movie?.poster_path} key={movie?.id} />))
-            }
-        </div>
+    <div className='bg-black bg-opacity-70 w-screen mt-6 box-border'>
+      <div className='bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
+        {movieResults.map((movies, index) => (
+          <div key={index} className='flex'>
+            <div className='flex overflow-x-auto w-screen scrollbar-hide'>
+              {movies.results.map((movie) => (
+                <Postercards img={movie?.poster_path} key={movie?.id} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
-           
-        </div>
-      ))}
     </div>
   );
 };
 
-export default GptSuggesions;
+export default GptSuggestions;
 
-
-// results
-// : 
-// Array(2)
-// 0
-// : 
-// adult
-// : 
-// false
-// backdrop_path
-// : 
-// "/gKuvFZJFgI8STwDqDtFIT4OfA6Z.jpg"
-// genre_ids
-// : 
-// (2) [10749, 18]
-// id
-// : 
-// 11854
-// original_language
-// : 
-// "hi"
-// original_title
-// : 
-// "कुछ कुछ होता है"
-// overview
-// : 
-// "Per her mother's last wish, an 8 year old girl sets out to reunite her father with his college best friend who was in love with him."
-// popularity
-// : 
-// 18.759
-// poster_path
-// : 
-// "/5FmtHHDGPofW5Zjns1EM1D8503c.jpg"
-// release_date
-// : 
-// "1998-10-16"
-// title
-// : 
-// "Kuch Kuch Hota Hai"
-// video
-// : 
-// false
-// vote_average
-// : 
-// 7.492
-// vote_count
-// : 
-// 360
