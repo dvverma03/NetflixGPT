29c319c4d8e0139d53b616405a09540518bbd62c
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-02-15 04:34:00 +0530

    Update Postercards.js

--- src/components/Postercards.js
+++ src/components/Postercards.js
@@ -14,16 +14,19 @@ const Postercards = (props) => {
      navigate("/videoplay")
   }
   
+  if(!props.poster_path) return null;
 
   return (
     <div className='z-10 w-36 md:w-44 mx-2 my-4 bg-cover hover:w-40 md:hover:w-48 hover:mx-0 hover:my-0 cursor-pointer'>
-        <img className='rounded-lg' src={Imag_URL + props.poster_path
+        {props?.poster_path && <img className='rounded-lg' src={Imag_URL + props.poster_path
 } alt="" key={props.key} 
         onClick={onClickHandler}
-        />
+        />}
     </div>
   )
 }
 
 export default Postercards
+
+
 
