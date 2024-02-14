2ac70e9a6d7f3e7a8a5d405f4ae4e485aa940bb4
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-02-15 04:27:24 +0530

    Update gptSuggesions.js

--- src/components/gptSuggesions.js
+++ src/components/gptSuggesions.js
@@ -1,23 +1,27 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Postercards from './Postercards';
+
 const GptSuggestions = () => {
   const { movieResults } = useSelector((store) => store.gpt);
   if (!movieResults) return null;
   return (
-    <div className='bg-black bg-opacity-70 w-screen mt-6 box-border'>
-      <div className='bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
-        {movieResults.map((movies, index) => (
-          <div key={index} className='flex'>
-            <div className='flex overflow-x-auto w-screen scrollbar-hide'>
-              {movies.results.map((movie) => (
-                <Postercards img={movie?.poster_path} key={movie?.id} />
-              ))}
-            </div>
-          </div>
-        ))}
+    <div className=' bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
+      {movieResults.map((movies, index) => (
+        <div key={index} className='flex '>
+             <div className='flex overflow-x-auto w-screen scrollbar-hide' >
+        <div className='flex'>
+              {
+                movies.results.map((movie)=>(<Postercards {...movie} />))
+            }
+        </div>
       </div>
+           
+        </div>
+      ))}
     </div>
   );
 };
 
 export default GptSuggestions;
+
