38c8ac78cc41dedd50f39b9bb69b1e8e3d7541a7
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-03-09 16:58:19 +0530

    Update gptSuggesions.js

--- src/components/gptSuggesions.js
+++ src/components/gptSuggesions.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Postercards from './Postercards';
+import SchimmerUI from "./schimmerUI/SchimmerUI.js"
 
-const GptSuggestions = () => {
+const GptSuggestions = ({toggle}) => {
   const { movieResults } = useSelector((store) => store.gpt);
-  if (!movieResults) return null;
   return (
     <div className=' bg-black bg-opacity-70 w-screen mt-[250px] md:mt-6 box-border'>
-      {movieResults.map((movies, index) => (
+
+      {
+        (!movieResults)?( toggle && <div className="my-4 mx-3 md:m-6 p-3 md:p-4 bg-black bg-opacity-80"><SchimmerUI /><SchimmerUI /><SchimmerUI /></div>):
+        <>
+        {movieResults.map((movies, index) => (
         <div key={index} className='flex '>
              <div className='flex overflow-x-auto w-screen scrollbar-hide' >
         <div className='flex'>
@@ -19,6 +23,10 @@ const GptSuggestions = () => {
            
         </div>
       ))}
+        </>
+      }
+
+
     </div>
   );
 };
