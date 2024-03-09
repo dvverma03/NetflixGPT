8f84a8ab0ccadc404b37987a87593b62194150fd
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-03-09 21:18:51 +0530

    Update SchimmerUI.js

--- src/components/schimmerUI/SchimmerUI.js
+++ src/components/schimmerUI/SchimmerUI.js
@@ -7,7 +7,7 @@ const SchimmerUI = () => {
             <h1 className="text-xl md:text-4xl md:py-2 text-white">
                 Loading...
             </h1>
-            <div className="flex z-10 w-36 md:w-44 mx-2 my-4 bg-cover hover:w-40 md:hover:w-48 hover:mx-0 hover:my-0 cursor-pointer">
+            <div className="flex overflow-x-scroll my-2 no-scrollbar">
                 <div className="flex">
                     {noOfCards.map((card, index) => <ShimmerCards key={index} />)}
                 </div>
